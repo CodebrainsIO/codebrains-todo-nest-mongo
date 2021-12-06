17a7123c114cd26dc59554e4aa25bbcab9e9e235
James Coonce <coonce.james@codebrains.io>
2021-12-06 10:16:57 -0500

    Init Commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    TodosModule,
+    MongooseModule.forRoot('mongodb://localhost/codebrains'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/todos/schemas/todo.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TodoDocument = Todo & Document;
+
+@Schema({
+  toJSON: {
+    getters: true,
+    virtuals: true,
+  },
+})
+export class Todo {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  completed: boolean;
+}
+
+const TodoSchema = SchemaFactory.createForClass(Todo);
+
+TodoSchema.virtual('id').get(function (this: TodoDocument) {
+  return this._id.toHexString();
+});
+
+export { TodoSchema };
